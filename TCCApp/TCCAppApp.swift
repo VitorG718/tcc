31c625fcbb97cc7f722fbb7c7c0b685d5a434a4d
Vitor Souza <vitorx1280@gmail.com>
2022-09-14 20:44:19 -0300

    Initial Commit

---
+++ TCCApp/TCCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCCAppApp.swift
+//  TCCApp
+//
+//  Created by Vitor Souza on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TCCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
