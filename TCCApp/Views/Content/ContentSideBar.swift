75c47881738759d67eec09607c2e0f146519366b
Vitor Souza <vitorx1280@gmail.com>
2022-11-30 23:28:51 -0300

    feat: add project name to sidebar

--- TCCApp/Views/Content/ContentSideBar.swift
+++ TCCApp/Views/Content/ContentSideBar.swift
@@ -12,7 +12,34 @@ struct ContentSideBar: View {
     let proxy: GeometryProxy
     
     var body: some View {
-        VStack(spacing: .zero) {
+        VStack(alignment: .trailing, spacing: .zero) {
+            VStack(alignment: .leading, spacing: .zero) {
+                Text("NOME DO\nPROJETO")
+                    .font(.system(
+                        size: .height(40, in: proxy, min: 20),
+                        weight: .bold
+                    ))
+                    .foregroundColor(Color(token: .activeGreen))
+                    .overlay {
+                        VStack {
+                            Spacer()
+                            Rectangle()
+                                .fill(Color(token: .activeGreen))
+                                .frame(height: 1)
+                        }
+                    }
+                
+                Text("by Company X")
+                    .font(.system(
+                        size: .height(20, in: proxy, min: 10),
+                        weight: .ultraLight
+                    ))
+                    .foregroundColor(.secondary)
+                    .padding(.top, .height(16, in: proxy, min: 5))
+            }
+            .padding(.top, .height(100, in: proxy, min: 50))
+            .padding(.trailing, .width(32, in: proxy, min: 16))
+            
             Spacer()
             
             VStack(spacing: .height(40, in: proxy, min: 15)) {
@@ -46,5 +73,6 @@ struct ContentSideBar: View {
 struct ContentSideBar_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .frame(width: 1080, height: 675)
     }
 }
